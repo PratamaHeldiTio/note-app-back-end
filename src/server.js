da27143e563bd98d60296a6ccda790d9ca4dff73
pratamahelditio <pratamahelditio@gmail.com>
2021-09-29 22:16:07 +0700

    feat: implemented plugin users with validation

--- src/server.js
+++ src/server.js
@@ -2,10 +2,14 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./sevices/postgres/NotesService');
-const notesValidator = require('./validator/notes/index');
+const notesValidator = require('./validator/notes');
+const users = require('./api/users');
+const UsersService = require('./sevices/postgres/UsersService');
+const usersValidator = require('./validator/user');
 
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -17,13 +21,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: notesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: notesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: usersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
--- src/sevices/postgres/UsersService.js
+++ src/sevices/postgres/UsersService.js
@@ -6,13 +6,13 @@ const NotFoundError = require('../../exceptions/NotFoundError');
 
 class UsersService {
   constructor() {
-    this._pool = Pool;
+    this._pool = new Pool();
   }
 
   async addUser({ username, password, fullname }) {
     await this.verifyNewUsername(username);
 
-    const id = `user-${nanoid(12)}`;
+    const id = `user-${nanoid(11)}`;
     const hashedPassword = await bcrypt.hash(password, 10);
     const query = {
       text: 'INSERT INTO users VALUES($1, $2, $3,  $4) RETURNING id',
@@ -30,7 +30,7 @@ class UsersService {
 
   async verifyNewUsername(username) {
     const query = {
-      text: 'SELECT username FROM users WHERE username  = $1',
+      text: 'SELECT username FROM users WHERE username = $1',
       values: [username],
     };
 
