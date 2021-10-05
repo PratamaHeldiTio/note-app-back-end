4949b36e2ef2792eb3da8245b62e95c19ac80c7f
pratamahelditio <pratamahelditio@gmail.com>
2021-10-05 21:50:23 +0700

    feat: authentication implemented

---
+++ src/api/authentications/Handler.js
@@ -0,0 +1,121 @@
+const autoBind = require('auto-bind');
+const ClientError = require('../../exceptions/ClientError');
+
+class AuthenticationsHandler {
+  constructor(authenticationsService, usersService, tokenManager, validator) {
+    this._authenticationsService = authenticationsService;
+    this._usersService = usersService;
+    this._tokenManager = tokenManager;
+    this._validator = validator;
+    autoBind(this);
+  }
+
+  async postAuthenticationHandler({ payload }, h) {
+    try {
+      this._validator.validatePostAuthenticationPayload(payload);
+      const id = await this._usersService.verifyUserCredential(payload);
+
+      const accessToken = this._tokenManager.generateAccessToken({ id });
+      const refreshToken = this._tokenManager.generateRefreshToken({ id });
+
+      await this._authenticationsService.addRefreshToken(refreshToken);
+
+      const response = h.response({
+        status: 'success',
+        message: 'Authentication berhasil ditambahkan',
+        data: {
+          accessToken,
+          refreshToken,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  async putAuthenticationHandler({ payload }, h) {
+    try {
+      this._validator.validatePutAuthenticationPayload(payload);
+      await this._authenticationsService.verifyRefreshToken(payload.refreshToken);
+      const { id } = this._tokenManager.verifyRefreshToken(payload.refreshToken);
+      const accessToken = this._tokenManager.generateAccessToken({ id });
+
+      return {
+        status: 'success',
+        message: 'Access Token berhasil diperbarui',
+        data: {
+          accessToken,
+        },
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  async deleteAuthenticationHandler({ payload }, h) {
+    try {
+      this._validator.validateDeleteAuthenticationPayload(payload);
+      await this._authenticationsService.verifyRefreshToken(payload.refreshToken);
+      await this._authenticationsService.deleteRefreshToken(payload.refreshToken);
+
+      return {
+        status: 'success',
+        message: 'Refresh token berhasil dihapus',
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+}
+
+module.exports = AuthenticationsHandler;
--- src/server.js
+++ src/server.js
@@ -1,15 +1,27 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
+
+// notes
 const notes = require('./api/notes');
 const NotesService = require('./sevices/postgres/NotesService');
 const notesValidator = require('./validator/notes');
+
+// users
 const users = require('./api/users');
 const UsersService = require('./sevices/postgres/UsersService');
 const usersValidator = require('./validator/user');
 
+// authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./sevices/postgres/AuthenticationService');
+const authenticationsValidator = require('./validator/authentication');
+const tokenManager = require('./token/tokenManager');
+
 const init = async () => {
   const notesService = new NotesService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -19,6 +31,27 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  // definisikan strategy authentication jwt
+  server.auth.strategy('notesapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: { id: artifacts.decoded.payload.id },
+    }),
   });
 
   await server.register([
@@ -34,6 +67,15 @@ const init = async () => {
       options: {
         service: usersService,
         validator: usersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager,
+        validator: authenticationsValidator,
       },
     },
   ]);
